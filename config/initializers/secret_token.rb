cd85f6e2787a0969039b0a08f5b5901c6ca99237
Chrissy <chris@rapgenius.com>
2014-03-15 21:44:55 -0400

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Tuxno2::Application.config.secret_key_base = '6b5d1992036e47a8aec55d780911f01a699dc26c0ea78d1b93f81075cf017526d1afd0f635f587fe38d2e1fcea64056edeb3961655723db72080b9f347dd2e30'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Tuxno2::Application.config.session_store :cookie_store, key: '_tuxno2_session'
