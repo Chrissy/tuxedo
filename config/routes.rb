975d869f00eaa40881d9948361ecc60da106171a
Chrissy <chris@rapgenius.com>
2014-03-16 17:31:54 -0400

    add debugger to gemfile

--- config/routes.rb
+++ config/routes.rb
@@ -8,6 +8,9 @@ Tuxno2::Application.routes.draw do
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
 
+  get 'new' => 'recipes#new' 
+  resources :recipes
+
   # Example of named route that can be invoked with purchase_url(id: product.id)
   #   get 'products/:id/purchase' => 'catalog#purchase', as: :purchase
 
