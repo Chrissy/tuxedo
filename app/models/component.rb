bc24f769d2767e20ca291eb3820b3032829b67ac
Chrissy <chris@rapgenius.com>
2014-04-12 14:39:38 -0400

    turn markdown component links into real links

--- app/models/component.rb
+++ app/models/component.rb
@@ -1,3 +1,11 @@
 class Component < ActiveRecord::Base
 
+def url
+  "/components/#{id}"
+end
+
+def link
+  "<a href='#{url}'>#{name}</a>"
+end
+
 end
--- app/models/recipe.rb
+++ app/models/recipe.rb
@@ -1,3 +1,15 @@
 class Recipe < ActiveRecord::Base
 
+  def recipe_to_html
+    markdown_to_html(recipe).html_safe
+  end
+
+  private
+
+  def markdown_to_html(md)
+    md.gsub(/\:\[(.*?)\]/) do |*|
+      Component.find_or_create_by(name: $1).link
+    end
+  end
+
 end
