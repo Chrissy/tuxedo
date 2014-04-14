25525258d9f8c5cdf51f5f6028416a148ade0b5a
Chrissy <chris@rapgenius.com>
2014-04-14 18:57:14 -0400

    compass and description to html

--- Gemfile
+++ Gemfile
@@ -9,6 +9,7 @@ gem 'debugger'
 
 # Use SCSS for stylesheets
 gem 'sass-rails', '~> 4.0.0'
+gem 'compass-rails'
 
 # Use Uglifier as compressor for JavaScript assets
 gem 'uglifier', '>= 1.3.0'
--- app/models/recipe.rb
+++ app/models/recipe.rb
@@ -3,26 +3,28 @@ require 'component.rb'
 class Recipe < ActiveRecord::Base
   serialize :components, Array
 
+  def markdown
+    Redcarpet::Markdown.new(Redcarpet::Render::HTML.new, extensions = {})
+  end
+
   def recipe_to_html
-    markdown_to_html(recipe).html_safe
+    markdown_to_html_with_components(recipe).html_safe
+  end
+
+  def description_to_html
+    markdown.render(description).html_safe
   end
 
   private
 
-  def markdown_to_html(md)
+  def markdown_to_html_with_components(md)
     component_list = []
-    markdown = Redcarpet::Markdown.new(Redcarpet::Render::HTML.new, extensions = {})
     md.gsub!(/\:\[(.*?)\]/) do |*|
       component = Component.find_or_create_by(name: $1)
       component_list << component
       component.link
     end
-    md.gsub!(/\#\[(.*?)\]/) do |*|
-      label = $1
-      "<div class='section-label #{label.gsub(/[ ]/,"-")}'>#{label}</div>"
-    end
     self.update_attribute(:components, component_list)
     markdown.render(md)
   end
-
 end
