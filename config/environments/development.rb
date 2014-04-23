58d9a01a309acdabc9e9ef7872b758a0a2d8878c
Chrissy <chris@rapgenius.com>
2014-04-23 01:53:17 -0400

    debug mode should be on in dev

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -26,5 +26,5 @@ Tuxno2::Application.configure do
   # Debug mode disables concatenation and preprocessing of assets.
   # This option may cause significant delays in view rendering with a large
   # number of complex assets.
-  config.assets.debug = false
+  config.assets.debug = true
 end
