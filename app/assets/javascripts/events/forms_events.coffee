2915af3955a376627cb29c994a8f80be2fe49e33
Chrissy <chris@rapgenius.com>
2014-05-27 23:49:15 -0400

    user links that only show up if you are logged in!

--- app/assets/javascripts/events/forms_events.coffee
+++ app/assets/javascripts/events/forms_events.coffee
@@ -1,3 +1,10 @@
 $ ->
   window.form = new Form('.autocomplete')
 
+  $(".authenticate").each(->
+    $this = $(this)
+    $.get('/authenticate_before_render').promise().then((data) ->
+      $this.addClass("authenticated") if Boolean(data)
+    )
+  )
+
--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,4 +3,8 @@ class ApplicationController < ActionController::Base
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
   before_action :authenticate_user!
+
+  def authenticate_before_render
+    render inline: user_signed_in?.to_s
+  end
 end
