41a00e17bf9879d82aa4059b09b65b03f98c3d6f
Chrissy Clark <chris@rapgenius.com>
2014-12-07 21:35:14 -0500

    only show the alert after a click, and don't store visits after 5

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,13 +2,17 @@ class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
-  before_action :authenticate_user!, :set_sharing_cookie
+  before_action :set_sharing_cookie
 
   def authenticate_before_render
     render inline: user_signed_in?.to_s
   end
   
   def set_sharing_cookie
-    cookies[:visit_count] = { value: cookies[:visit_count].to_i + 1, expires: 1.days.from_now }
+    current_count = cookies[:visit_count].to_i
+    if current_count < 5
+      new_count = current_count + 1
+      cookies[:visit_count] = { value: new_count, expires: 3.days.from_now }
+    end
   end
 end
