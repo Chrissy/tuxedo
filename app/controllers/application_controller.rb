04cbb94401b18b7b5713cdb8532b70221472c115
Chrissy Clark <chris@rapgenius.com>
2014-12-07 22:18:14 -0500

    re-add authenticate user

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,7 +2,7 @@ class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
-  before_action :set_sharing_cookie
+  before_action :authenticate_user!, :set_sharing_cookie
 
   def authenticate_before_render
     render inline: user_signed_in?.to_s
