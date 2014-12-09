53a01ca8126396d6b15b00b506e2a465c61d6da7
Chrissy Clark <chris@rapgenius.com>
2014-12-08 20:17:05 -0500

    setup windows class and fix numbers to get started!

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,6 +3,11 @@ class ApplicationController < ActionController::Base
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
   before_action :authenticate_user!, :set_sharing_cookie
+  helper_method :windows?
+  
+  def windows?
+    request.user_agent =~ /.*Windows/
+  end
   
   def set_sharing_cookie
     current_count = cookies[:visit_count].to_i
