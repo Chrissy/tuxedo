a9b48aedb5b23faa6192a5fcbe6fd88a5d8a0f40
chrissy <chrissy@chrissys-MacBook-Pro.local>
2015-06-16 00:32:30 -0400

    remove super unecessary timestamp updates

--- app/controllers/recipes_controller.rb
+++ app/controllers/recipes_controller.rb
@@ -21,7 +21,6 @@ class RecipesController < ApplicationController
   def update
     recipe = Recipe.find(params[:id])
     recipe.update_attributes(recipe_params)
-    recipe.update_attribute(:last_updated, DateTime.now)
     recipe.convert_recipe_to_html_and_create_relationships
     recipe.make_my_number_last! if params[:make_my_number_last]
     Relationship.touch_all_parents_of(recipe)
@@ -31,7 +30,6 @@ class RecipesController < ApplicationController
 
   def create
     recipe = Recipe.create(recipe_params)
-    recipe.update_attribute(:last_updated, DateTime.now)
     recipe.convert_recipe_to_html_and_create_relationships
     recipe.store_recommends
     redirect_to action: "show", id: recipe.id
