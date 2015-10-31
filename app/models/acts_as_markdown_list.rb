e87e91f9c0913d82c34fd3963aa83616208e93fb
chrissy <chris@genius.com>
2015-10-30 23:11:37 -0400

    add other methods

--- app/models/acts_as_markdown_list.rb
+++ app/models/acts_as_markdown_list.rb
@@ -1,7 +1,35 @@
 module ActsAsMarkdownList
   module ActsAsMethods
+
     def acts_as_markdown_list(options = {})
       has_many :relationships, as: :relatable, dependent: :destroy
+      after_save :create_relationships
+
+      define_method(:elements) do
+        relationships.map do |rel|
+          rel.expand || rel.child
+        end.flatten.uniq.keep_if { |element| element.try(:published?) }
+      end
+
+      define_method(:recipes) do
+        relationships.select{ |rel| rel.child_type == "Recipe" }.map(&:child).keep_if { |element| element.published? }
+      end
+
+      define_method(:create_relationships) do
+        delete_and_save_relationships if content_as_markdown_changed?
+      end
+
+      define_method(:delete_and_save_relationships) do
+        if relationships_from_markdown.present? && self.id
+          relationships.delete_all
+          Relationship.create(relationships_from_markdown)
+        end
+      end
+
+      define_method(:relationships_from_markdown) do
+        CustomMarkdown.relationships_from_markdown(self, content_as_markdown, :in_list_content)
+      end
+
     end
   end
 end
--- app/models/list.rb
+++ app/models/list.rb
@@ -9,32 +9,7 @@ class List < ActiveRecord::Base
 
   friendly_id :custom_name, use: :slugged
 
-  after_save :create_relationships
-
-  serialize :element_ids, Array
-
   acts_as_markdown_list
-
-  def elements
-    relationships.map do |rel|
-      rel.expand || rel.child
-    end.flatten.uniq.keep_if { |element| element.try(:published?) }
-  end
-
-  def create_relationships
-    delete_and_save_relationships if content_as_markdown_changed?
-  end
-
-  def delete_and_save_relationships
-    if relationships_from_markdown.present? && self.id
-      relationships.delete_all
-      Relationship.create(relationships_from_markdown)
-    end
-  end
-
-  def relationships_from_markdown
-    CustomMarkdown.relationships_from_markdown(self, content_as_markdown, :in_list_content)
-  end
 
   def url
     "/list/#{slug}"
@@ -66,10 +41,6 @@ class List < ActiveRecord::Base
 
   def count_for_display
     "#{elements.count} cocktails"
-  end
-
-  def recipes
-    relationships.select{ |rel| rel.child_type == "Recipe" }.map(&:child).keep_if { |element| element.published? }
   end
 
   def link
