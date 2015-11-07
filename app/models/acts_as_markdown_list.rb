b0b81e4aafbabb4a922b79e573c4d517f9c29f35
chrissy <chris@genius.com>
2015-11-07 00:12:03 -0600

    don't use define_method because c'mon that's weird

--- app/models/acts_as_markdown_list.rb
+++ app/models/acts_as_markdown_list.rb
@@ -1,35 +1,39 @@
 module ActsAsMarkdownList
   module ActsAsMethods
-
     def acts_as_markdown_list(options = {})
+      include ActsAsMarkdownList
+
       has_many :relationships, as: :relatable, dependent: :destroy
       after_save :create_relationships
-
-      define_method(:elements) do
-        relationships.map do |rel|
-          rel.expand || rel.child
-        end.flatten.uniq.keep_if { |element| element.try(:published?) }
-      end
-
-      define_method(:recipes) do
-        relationships.select{ |rel| rel.child_type == "Recipe" }.map(&:child).keep_if { |element| element.published? }
-      end
-
-      define_method(:create_relationships) do
-        delete_and_save_relationships if content_as_markdown_changed?
-      end
-
-      define_method(:delete_and_save_relationships) do
-        if relationships_from_markdown.present? && self.id
-          relationships.delete_all
-          Relationship.create(relationships_from_markdown)
-        end
-      end
-
-      define_method(:relationships_from_markdown) do
-        CustomMarkdown.relationships_from_markdown(self, content_as_markdown, :in_list_content)
-      end
-
     end
+  end
+
+  def self.included(base)
+    base.extend ClassMethods
+  end
+
+  def elements
+    relationships.map do |rel|
+      rel.expand || rel.child
+    end.flatten.uniq.keep_if { |element| element.try(:published?) }
+  end
+
+  def recipes
+    relationships.select{ |rel| rel.child_type == "Recipe" }.map(&:child).keep_if { |element| element.published? }
+  end
+
+  def create_relationships
+    delete_and_save_relationships if content_as_markdown_changed?
+  end
+
+  def delete_and_save_relationships
+    if relationships_from_markdown.present? && self.id
+      relationships.delete_all
+      Relationship.create(relationships_from_markdown)
+    end
+  end
+
+  def relationships_from_markdown
+    CustomMarkdown.relationships_from_markdown(self, content_as_markdown, :in_list_content)
   end
 end
