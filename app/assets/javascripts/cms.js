0e1028d064c49d50cbfb13b2183fadef48d7ef8a
chrissy <chris@genius.com>
2017-04-02 11:02:32 -0500

    seperate cms javascript and content javascript, since we're gonna be adding a bunch of crap to replace filepicker. add in the lamest most follow-the-tutorial s3 file uploader

---
+++ app/assets/javascripts/cms.js
@@ -0,0 +1,77 @@
+import $ from 'jquery';
+import Form from './classes/form.js';
+import aws from 'aws-sdk';
+
+var bucketName = 'chrissy-tuxedo-no2';
+aws.config.region = 'us-east-2';
+
+var fbUserId;
+var bucket = new AWS.S3({
+  params: {
+    Bucket: bucketName
+  }
+});
+
+const uploadImage = function(file) {
+  FB.login(function(response){
+    bucket.config.credentials = new AWS.WebIdentityCredentials({
+      ProviderId: 'graph.facebook.com',
+      RoleArn: 'arn:aws:iam::707718423679:role/tuxuploader',
+      WebIdentityToken: response.authResponse.accessToken
+    });
+
+    const params = {
+      Key: 'facebook-' + response.authResponse.userID + '/' + file.name,
+      ContentType: file.type,
+      Body: file,
+      ACL: 'public-read'
+    }
+
+    bucket.putObject(params, function (err, data) {
+      console.log(data)
+      bucket.listObjects({Prefix: 'facebook-' + response.authResponse.userID}, function(err, data){
+        if (err) console.log(err)
+        console.log(data.Contents);
+      });
+    });
+  });
+}
+
+window.fbAsyncInit = function() {
+   FB.init({
+     appId: '1514166265511110',
+     xfbml: true,
+     version: 'v2.8'
+   });
+ };
+
+ (function(d, s, id){
+    var js, fjs = d.getElementsByTagName(s)[0];
+    if (d.getElementById(id)) {return;}
+    js = d.createElement(s); js.id = id;
+    js.src = "//connect.facebook.net/en_US/sdk.js";
+    fjs.parentNode.insertBefore(js, fjs);
+  }(document, 'script', 'facebook-jssdk'));
+
+$(() => {
+  const forms = [];
+
+  $('.autocomplete').each(function() {
+    let form = new Form(this);
+    forms.push(form);
+  });
+
+  $("#upload-cover-photo").on("change", function(){
+    const file = $(this).get(0).files[0];
+    uploadImage(file);
+  })
+
+  // $(".clear_image").on("click", function() {
+  //   $(this).addClass("cleared").parent().find('[type="filepicker"]').attr("value", "");
+  //   return false;
+  // });
+
+  $(".show-extra-options").on("click", function() {
+    $(this).siblings(".extra-options").toggle();
+  });
+});
