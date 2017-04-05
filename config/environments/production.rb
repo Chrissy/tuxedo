a65b4e9889b646d2142434dbf90ae962475c04fe
chrissy <chris@genius.com>
2017-04-05 14:22:43 -0500

    actually turn off the asset pipeline in production

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -21,18 +21,18 @@ Tuxno2::Application.configure do
   # config.action_dispatch.rack_cache = true
 
   # Disable Rails's static asset server (Apache or nginx will already do this).
-  config.serve_static_assets = true
+  #config.serve_static_assets = true
 
   # Compress JavaScripts and CSS.
-  config.assets.js_compressor = :uglifier
+  #config.assets.js_compressor = :uglifier
   # config.assets.css_compressor = :sass
 
   # Do not fallback to assets pipeline if a precompiled asset is missed.
-  config.assets.compile = false
-  config.assets.enabled = false
+  #config.assets.compile = false
+  #config.assets.enabled = false
 
   # Generate digests for assets URLs.
-  config.assets.digest = true
+  #config.assets.digest = true
 
   # Version of your assets, change this if you want to expire all your assets.
   config.assets.version = '1.0'
