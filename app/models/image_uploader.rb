9f4f4eed85a3f10ab959424fdcbfab1a25151c5a
Chrissy <christopherchristopherclark@gmail.com>
2019-11-17 00:20:48 -0800

    add image uploader tests

--- app/models/image_uploader.rb
+++ app/models/image_uploader.rb
@@ -4,16 +4,14 @@ require 'mini_magick'
 
 class ImageUploader
   def initialize(image)
-    image_env = JSON.load(File.read('images.json'))
     credentials = get_credentials()
     @path = image
     @s3 = Aws::S3::Client.new(region: 'us-east-2', credentials: credentials)
-    @newImage = MiniMagick::Image.open(image_env["bucket"] + image)
-    @sizes = image_env["sizes"]
+    @newImage = MiniMagick::Image.open(self.class.bucket + image)
   end
 
   def get_credentials()
-    if ENV['RAILS_ENV'] == 'development'
+    if ENV['RAILS_ENV'] == 'development' || ENV['RAILS_ENV'] == 'test'
       creds = JSON.load(File.read('secrets.json'))
       return Aws::Credentials.new(creds['AccessKeyId'], creds['SecretAccessKey'])
     else
@@ -21,19 +19,50 @@ class ImageUploader
     end
   end
 
+
+  def source_image
+    @newImage
+  end
+
+  def file_head(key)
+    @s3.head_object(bucket: 'chrissy-tuxedo-no2', key: key)
+  end
+
+  def self.sizes(size)
+    {
+      "largeCover" => "1500x861",
+      "mediumCover" => "500x287",
+      "thumb" => "100x100",
+      "pinterest" => "476x666",
+      "list" => "600x400"
+    }[size]
+  end
+
+  def self.bucket
+    "https://s3.us-east-2.amazonaws.com/chrissy-tuxedo-no2/"
+  end
+
   def upload()
-    @sizes.values.each do |size|
-      putImageWithSize(MiniMagick::Image.open(@newImage.path), size)
+    self.class.sizes.values.each do |size|
+      putImageWithSize(size)
     end
   end
 
-  def putImageWithSize(image, size)
-    image.combine_options do |i|
+  def resize_image(size)
+    @newImage.combine_options do |i|
       i.resize(size + "^")
       i.gravity("Center")
       i.extent(size)
     end
+  end
 
-    @s3.put_object(bucket: 'chrissy-tuxedo-no2', body: image.to_blob, key: size + @path)
+  def key(size)
+    size + @path
+  end
+
+  def putImageWithSize(size)
+    resized_image = resize_image(size)
+
+    @s3.put_object(bucket: 'chrissy-tuxedo-no2', body: resized_image.to_blob, key: key)
   end
 end
