f240b0d980394bddefe5f7f1d05670d292bacfeb
Chrissy <christopherchristopherclark@gmail.com>
2019-11-17 17:39:12 -0800

    remove dotenv stuff

--- webpack.config.js
+++ webpack.config.js
@@ -2,13 +2,6 @@ const path = require('path');
 const Dotenv = require('dotenv-webpack');
 
 module.exports = {
-  mode: (process.env['NODE_ENV'] || 'development'),
-  plugins: [
-    new Dotenv({
-      S3_KEY: true,
-      S3_SECRET: true,
-    })
-  ],
   entry: {
     application: './app/assets/javascripts/application.js',
     cms: './app/assets/javascripts/cms.js',
