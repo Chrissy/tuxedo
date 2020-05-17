026eb4275447ee9ca3000fa0ac215a80acd960fd
Chrissy <christopherchristopherclark@gmail.com>
2020-05-16 20:27:18 -0700

    reset reloacater on resize

--- app/assets/javascripts/classes/mobileRelocater.js
+++ app/assets/javascripts/classes/mobileRelocater.js
@@ -20,17 +20,26 @@ export default class MobileRelocater {
       toRelocate,
       relocateAfter,
       originalPreviousNode,
+      mode: null,
     });
 
+    this.set();
+    window.addEventListener("resize", this.set.bind(this));
+  }
+
+  set() {
     const mediaQuery = window.matchMedia(`(max-width: ${BREAKPOINT}px)`);
-    if (mediaQuery.matches) this.setMobile();
+    if (mediaQuery.matches && this.mode !== "mobile") return this.setMobile();
+    if (this.mode !== "desktop") return this.setDesktop();
   }
 
   setMobile() {
     this.relocateAfter.after(this.toRelocate);
+    this.mode = "mobile";
   }
 
   setDesktop() {
     this.originalPreviousNode.after(this.toRelocate);
+    this.mode = "desktop";
   }
 }
