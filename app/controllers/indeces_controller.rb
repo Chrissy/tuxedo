6e266672e7b5b4931e282b97b92221fc6fc29ea8
Chrissy <christopherchristopherclark@gmail.com>
2020-05-17 23:09:21 -0700

    fix index

--- app/controllers/indeces_controller.rb
+++ app/controllers/indeces_controller.rb
@@ -1,13 +1,15 @@
+# frozen_string_literal: true
+
 class IndecesController < ApplicationController
-  skip_before_action :authenticate_user!, only: [:index, :letter_index]
-  layout "application"
+  skip_before_action :authenticate_user!, only: %i[index letter_index]
+  layout 'application'
 
   def index
-    @elements = List.all_for_display.concat(Recipe.all_for_display).concat(Component.all_for_display).sort_by(&:name)
+    @elements = Recipe.all_for_display.to_a.concat(Component.all_for_display).sort_by(&:name)
   end
 
   def letter_index
-    @elements = List.get_by_letter(params[:letter]).concat(Recipe.get_by_letter(params[:letter])).concat(Component.get_by_letter(params[:letter]))
+    @elements = Recipe.get_by_letter(params[:letter]).concat(Component.get_by_letter(params[:letter]))
     render 'index'
   end
 end
