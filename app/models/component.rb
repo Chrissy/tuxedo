ab4c733677cf0c8dfd122bd391b24d6b46ce70a6
Chrissy <christopherchristopherclark@gmail.com>
2020-05-24 21:27:36 -0700

    ok why not this

--- app/models/component.rb
+++ app/models/component.rb
@@ -136,15 +136,15 @@ class Component < ActiveRecord::Base
   end
 
   def classic_recipes(count = 3)
-    all_elements.sort_by(&:created_at).reverse.sort_by { |a| a.classic? ? 0 : 1 }.uniq.slice(0, count)
+    all_elements.sort_by(&:created_at).reverse.sort_by { |a| a.classic? ? 0 : 1 }.uniq[0..count]
   end
 
   def latest_recipes(count = 3, start = 0)
-    all_elements.sort_by(&:created_at).reverse.slice(start, count)
+    all_elements.sort_by(&:created_at).reverse[start..start + count]
   end
 
   def original_recipes(count = 3)
-    all_elements.sort_by(&:created_at).reverse.sort_by { |a| a.original? ? 0 : 1 }.slice(0, count)
+    all_elements.sort_by(&:created_at).reverse.sort_by { |a| a.original? ? 0 : 1 }[0..count]
   end
 
   def featured_recipes(count = 3)
