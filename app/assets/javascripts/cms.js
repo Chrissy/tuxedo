ec9472533c64917f3871eee84099b85de93e8e99
Chrissy <christopherchristopherclark@gmail.com>
2020-11-09 22:49:21 -0800

    fix image clearing

--- app/assets/javascripts/cms.js
+++ app/assets/javascripts/cms.js
@@ -6,44 +6,40 @@ import ImageUploader from "./classes/imageUploader";
 $(() => {
   const forms = [];
 
-  $(".autocomplete").each(function() {
+  $(".autocomplete").each(function () {
     let form = new Form(this);
     forms.push(form);
   });
 
-  $(".clear_image").on("click", function() {
+  $(".clear_image").on("click", function () {
     $(this)
       .addClass("cleared")
       .parent()
-      .find("#image-filename")
+      .find(".image-filename")
       .attr("value", "");
     return false;
   });
 
-  $(".show-extra-options").on("click", function() {
-    $(this)
-      .siblings(".extra-options")
-      .toggle();
+  $(".show-extra-options").on("click", function () {
+    $(this).siblings(".extra-options").toggle();
   });
 
-  document.querySelectorAll(".tag-select").forEach(element => {
+  document.querySelectorAll(".tag-select").forEach((element) => {
     new TagSelect(element);
   });
 
   fetch("/image-upload-token")
-    .then(response => response.json())
+    .then((response) => response.json())
     .then((response, reject) => {
       if (reject) console.warn("something went wrong", reject);
       const uploader = new ImageUploader(response);
       const uploads = [];
 
-      $(".upload-cover-photo").on("change", function() {
+      $(".upload-cover-photo").on("change", function () {
         $("input[type='submit']").attr("disabled", true);
         uploads.push(
-          uploader.upload($(this).get(0).files[0]).then(file => {
-            $(this)
-              .siblings(".image-filename")
-              .val(file.name);
+          uploader.upload($(this).get(0).files[0]).then((file) => {
+            $(this).siblings(".image-filename").val(file.name);
           })
         );
         Promise.all(uploads).then(() => {
