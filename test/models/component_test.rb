7e61d173ccaaaa5480b01ab77186cf5e4a046bae
Chrissy <christopherchristopherclark@gmail.com>
2020-11-09 21:10:49 -0800

    fix tests

--- test/models/component_test.rb
+++ test/models/component_test.rb
@@ -5,7 +5,7 @@ class ComponentTest < ActiveSupport::TestCase
   def test_description_to_html
     testComponent = Component.find(1)
     description = testComponent.description_to_html
-    assert description == "<p>has subcomponent <h2><a href='/ingredients/gin'>old tom</a></h2> with some <a href='/ingredients/rye'>rye</a></p>\n"
+    assert description == "<p><span class='first-letter'>h</span>as subcomponent <h2 id='old-tom' class='subcomponent'>old tom • <a href='#table' data-table-link='old%20tom'>0 recipes »</a></h2> with some <a href=\"/ingredients/rye\">rye</a></p>\n"
   end
 
   def test_description_to_plain_text
